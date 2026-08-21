1139b20b35ece86eb1c44ec5292a1e1ae899ef82
Ashutosh Panwar <ashutoshpanwar07@gmail.com>
2026-08-21 11:55:03 +0530

    Fix Railway SMTP and proxy configuration

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,11 +1,19 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  connectionTimeout: 15000,
+  greetingTimeout: 15000,
+  socketTimeout: 20000,
 });
 
 const escapeHtml = (value) =>
